cd707198857aad1fbdb365c17ffd89d64094910f
Boris Arkenaar <boris@arkenaar.com>
2019-01-03 16:09:24 +0100

    Create function `addN`
    Demonstrate it in a component.

---
+++ src/AddN.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+interface IProps {
+  n: number
+}
+
+const addN = (n: number) => (m: number) => m + n;
+
+class AddN extends React.Component<IProps> {
+  public render() {
+    const {n} = this.props;
+    const addSpecific = addN(n);
+    const numbers = [-101, -100, -99, -50, -49, -11, -10, -9, -1, 0, 1, 9, 10, 11, 49, 50, 99, 100, 101]
+    return (
+      <div>
+        <h2>Add {n} to...</h2>
+        <ul>
+          { numbers.map((specific) => (
+            <li>...{specific} resolves to {addSpecific(specific)}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AddN;
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+
+import AddN from './AddN';
+
 import './App.css';
 
 import logo from './logo.svg';
@@ -14,6 +17,25 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <AddN n={-101} />
+        <AddN n={-100} />
+        <AddN n={-99} />
+        <AddN n={-49} />
+        <AddN n={-11} />
+        <AddN n={-10} />
+        <AddN n={-9} />
+        <AddN n={-5} />
+        <AddN n={-1} />
+        <AddN n={0} />
+        <AddN n={1} />
+        <AddN n={5} />
+        <AddN n={9} />
+        <AddN n={10} />
+        <AddN n={11} />
+        <AddN n={49} />
+        <AddN n={99} />
+        <AddN n={100} />
+        <AddN n={101} />
       </div>
     );
   }
