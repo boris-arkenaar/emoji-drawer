4f56ee9e08e7da43a67ecc88b4617a1346e3c45a
Boris Arkenaar <boris@arkenaar.com>
2019-01-08 12:46:21 +0100

    Remove unused test

--- src/EmojiDrawer.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as enzyme from 'enzyme';
-import * as React from 'react';
-
-import EmojiDrawer from './EmojiDrawer';
-
-it('renders the correct text when no enthusiasm level is given', () => {
-  const emojiDrawer = enzyme.shallow(<EmojiDrawer />);
-  expect(emojiDrawer.find("h2").text()).toContain('Emoji Drawer');
-});
